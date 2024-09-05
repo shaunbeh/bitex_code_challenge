b9eaefa3ef266eb711749977aab399bdc3552077
shaunbeh <shahin.behmand@gmail.com>
2024-09-05 20:38:42 +0330

    fix(search): coinlist fetch fix

--- src/store/zustandStore.ts
+++ src/store/zustandStore.ts
@@ -1,5 +1,4 @@
 // src/store.js
-import axios from 'axios';
 import { create } from 'zustand';
 
 import endpoints from '@/lib/endpoints';
@@ -19,11 +18,13 @@ const useLazyDataStore = create<CoinsListStore>((set, get) => ({
     try {
       const ONE_DAY_MS = 24 * 60 * 60 * 1000;
       const lastTime = get().lastFetchedTimestamp;
-      const shouldRefetch = !lastTime || Date.now() - lastTime > ONE_DAY_MS;
+      const hasData = get().coinsList?.length;
+      const shouldRefetch =
+        !hasData && (!lastTime || Date.now() - lastTime > ONE_DAY_MS);
 
       if (shouldRefetch) {
-        const response = await axios.get(endpoints.coins.coinsList.url);
-        const data = await response.data();
+        const response = await fetch(endpoints.coins.coinsList.url);
+        const data = await response.json();
         set({ coinsList: data, lastFetchedTimestamp: Date.now() });
       }
     } catch (error) {
